85860374feda8e2e88ed38dd24b9172bc2718d12
pexemo <67392584+pexemo@users.noreply.github.com>
2021-05-19 14:16:53 +0430

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // counter
 const blackCounterLable = document.querySelector('#blackC p');
 const whiteCounterLable = document.querySelector('#whiteC p');
@@ -15,14 +14,6 @@ const container = document.querySelector('#container');
 const blocks = document.querySelectorAll('.block');
 const nobat = document.querySelector('#nobat');
 let turnblack = false;
-
-if (window.matchMedia("(orientation: portrait)").matches) {
-    // you're in PORTRAIT mode
-    container.style.height = getComputedStyle(container).width;
-} else if (matchMedia("(orientation: landscape)").matches) {
-    // you're in LANDSCAPE mode
-    container.style.width = getComputedStyle(container).height;
-}
 
 blocks.forEach(e => {
     e.addEventListener('click', () => {
